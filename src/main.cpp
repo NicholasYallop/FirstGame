53a3f3f211c9a45656a52d71619cb370844c756c
NickYallop <nickyallop@googlemail.com>
2023-05-14 21:24:43 +0100

    can shoot a bullet

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 
 App app = App();
 Entity player = Entity();
+Entity bullet = Entity();
 
 int main(void){
     memset(&app, 0, sizeof(App)); // empties the memory occupied by the app variable
 	memset(&player, 0, sizeof(Entity));
+	memset(&bullet, 0, sizeof(Entity));
 
 	initSDL();
 
@@ -15,6 +17,12 @@ int main(void){
 	player.w = 100;
 	player.h = 100;
 
+	bullet.texture = loadTexture("gfx/gcse.png");
+	bullet.x = 100;
+	bullet.y = 100;
+	bullet.w = 10;
+	bullet.h = 10;
+
 	//TODO: collect garbage
 	//atexit(cleanup);
 
@@ -23,6 +31,9 @@ int main(void){
 		prepareScene();
 
 		doInput();
+
+		player.x += player.dx;
+		player.y += player.dy;
 
 		if (app.up)
 		{
@@ -44,8 +55,29 @@ int main(void){
 			player.x += 4;
 		}
 
+		if (app.fire && bullet.health == 0)
+		{
+			bullet.x = player.x;
+			bullet.y = player.y;
+			bullet.dx = 16;
+			bullet.dy = 0;
+			bullet.health = 1;
+		}
+
+		bullet.x += bullet.dx;
+		bullet.y += bullet.dy;
+
+		if (bullet.x > SCREEN_WIDTH)
+		{
+			bullet.health = 0;
+		}
+
 		blit(player.texture, player.x, player.y, player.w, player.h);
 
+		if (bullet.health > 0)
+		{
+			blit(bullet.texture, bullet.x, bullet.y, bullet.w, bullet.h);
+		}
 		presentScene();
 
 		SDL_Delay(16);
--- src/structs.h
+++ src/structs.h
@@ -2,16 +2,23 @@
 
 typedef struct {
 	public:
+		// SDL components
 		SDL_Renderer *renderer;
-		SDL_Window *window;	
+		SDL_Window *window;
+
+		// input flags	
 		int up;
 		int down;
 		int left;
 		int right;
+		int fire;
 } App;
 
 typedef struct {
 	int x, y;
 	int w, h;
+	int dx;
+	int dy;
+	int health;
 	SDL_Texture *texture;
 } Entity;
